c4145547ea1f30ea0e837629702c25aed1f57e71
Willian Rodrigues <willianrdriguesz@gmail.com>
2024-11-27 17:16:47 -0300

    feat(checkAcess): Add acesso de modulo no token jwt

--- backEnd/middlewares/checkAccessMiddleware.js
+++ backEnd/middlewares/checkAccessMiddleware.js
@@ -1,39 +1,22 @@
-const Perfil = require('../models/profileModel');
-const Modulos = require('../models/moduleModel');
-const PerfilModulos = require('../models/assignProfileModuleModel');
 
 async function verificarPermissao(req, res, next) {
     try {
-        const { perfil } = req.user; // Extraído do token JWT
-        console.log(perfil);
+        const { perfil, modulos } = req.user; // Extraído do token JWT
+        console.log(perfil, modulos);
         
         if (!perfil) {
             return res.status(401).json({ erro: 'Perfil do usuário não fornecido no token.' });
         }
 
-        // Verifica se o perfil do usuário existe no banco
-        const perfilUsuario = await Perfil.findOne({
-             where: { id: perfil } 
-        });
-
-        if (!perfilUsuario) {
-            return res.status(404).json({ erro: 'Perfil do usuário não encontrado.' });
+        if (!modulos || modulos.length == 0) {
+            return res.status(401).json({ erro: 'Nenhum módulo associado ao perfil no token.' });
         }
 
         // Extrai o nome do módulo solicitado da URL
         const moduloSolicitado = req.originalUrl.split('/')[1];
-        const modulo = await Modulos.findOne({ where: { nome: moduloSolicitado } });
 
-        if (!modulo) {
-            return res.status(404).json({ erro: `Módulo '${moduloSolicitado}' não encontrado.` });
-        }
-
-        // Verifica se o perfil do usuário tem acesso ao módulo
-        const acesso = await PerfilModulos.findOne({
-            where: { perfil_id: perfilUsuario.id, modulo_id: modulo.id },
-        });
-
-        if (!acesso) {
+        // Verifica se o módulo solicitado está na lista de módulos do perfil (proveniente do token)
+        if (!modulos.includes(moduloSolicitado)) {
             return res.status(403).json({ erro: `Acesso negado ao módulo '${moduloSolicitado}'.` });
         }
 
