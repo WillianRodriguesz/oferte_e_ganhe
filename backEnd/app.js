e73903c3de4eb2f1c52eff6da4f3bdb06cbaef64
Willian Rodrigues <willianrdriguesz@gmail.com>
2024-11-28 15:42:56 -0300

    fix(login): bug de autenticação no login

--- backEnd/app.js
+++ backEnd/app.js
@@ -16,17 +16,19 @@ const userRoutes = require('./routes/userRoutes');
 const moduleRoutes = require('./routes/moduleRoutes');
 const andressRoutes = require('./routes/adressRoutes');
 const assignProfileModuleRoutes = require('./routes/assignProfileModuleRoutes');
+const home = require('./routes/homeRoutes');
 const app = express();
 
 // Middleware global de CORS
 app.use(corsMiddleware);
 
 // Middleware para servir arquivos estáticos (CSS e JS)
-app.use(express.static(path.join(__dirname, '../frontend/public')));
+app.use(express.static(path.join(__dirname, '../frontEnd/src')));
 app.use(express.json()); // Necessário para interpretar JSON
 app.use(express.urlencoded({ extended: true })); // Necessário para interpretar dados URL-encoded
 
-// Rotas para as páginas HTML
+app.use('/', home)
+app.use('/', andressRoutes);
 app.use('/', loginRoutes);
 app.use('/', reportingRoutes);
 app.use('/', stockRoutes);
--- frontEnd/src/scripts/login.js
+++ frontEnd/src/scripts/login.js
@@ -1,47 +1,31 @@
+
+import { enviarLogin } from '../services/loginService.js';
 // Pega os elementos do formulário
 const emailInput = document.getElementById('email');
 const senhaInput = document.getElementById('senha');
 const btnLogin = document.getElementById('btnLogin');
 
-// Função para enviar os dados de login para a API
-async function enviarLogin(email, senha) {
-    try {
-        const response = await fetch('http://localhost:3000/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ email, senha }),
-            credentials: 'include',  // Se precisar enviar cookies
-        });
-
-        const data = await response.json();
-
-        if (response.ok) {
-            // Sucesso no login - Redirecionar ou mostrar mensagem de sucesso
-            console.log('Login bem-sucedido:', data);
-            // Aqui você pode redirecionar para outra página ou armazenar dados do usuário
-        } else {
-            // Falha no login
-            console.error('Erro no login:', data);
-            alert('E-mail ou senha incorretos.');
-        }
-    } catch (error) {
-        console.error('Erro ao fazer login:', error);
-        alert('Erro ao tentar fazer login. Tente novamente.');
-    }
-}
-
-// Adiciona o evento de clique no botão de login
-btnLogin.addEventListener('click', (e) => {
+// Função para lidar com o clique no botão de login
+async function validacaoLogin(e) {
     e.preventDefault(); // Evita o comportamento padrão do formulário (recarregar a página)
     const email = emailInput.value;
     const senha = senhaInput.value;
 
     if (email && senha) {
         // Chama a função de enviar o login para a API
-        enviarLogin(email, senha);
+        const result = await enviarLogin(email, senha);
+
+        if (result.success) {
+            console.log('Login bem-sucedido:', result.data);
+            window.location.href = '../../pages/login/home.html';
+        } else {
+            console.error('Erro no login:', result.message);
+            alert(result.message || 'E-mail ou senha incorretos.');
+        }
     } else {
         alert('Preencha todos os campos.');
     }
-});
+}
+
+// Adiciona o evento de clique no botão de login
+btnLogin.addEventListener('click', validacaoLogin);
